cb237e6864eedbc7d277a8da747930480dd758df
hsquared <h.hristov@securelogiq.com>
2025-03-10 22:27:52 +0200

    changed the lsp config to go through the completion list with Tab and Shift+Tab and select with Enter

--- lua/hsq/lazy/lsp.lua
+++ lua/hsq/lazy/lsp.lua
@@ -65,17 +65,16 @@ return {
                     require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
                 end,
             },
-            --[[mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }), ]]--
+            mapping = cmp.mapping.preset.insert({
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
                 { name = 'buffer' },
+                { name = 'path' },
+                { name = 'luasnip' }, -- For luasnip users.
             })
         })
 
